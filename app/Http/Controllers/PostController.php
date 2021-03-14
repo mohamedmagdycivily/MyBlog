c98f204ffb11b5455c8cc9999963f072bad8ad75
mohamedmagdycivily <mohamedmagdycivily@gmail.com>
2021-03-14 19:38:54 +0200

    lab2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request; //NOt used here 
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = [
-            ['id' => 1, 'title' => 'Laravel', 'description' => 'This Is description', 'posted_by' => 'Ahmed', 'created_at' => '2021-03-13'],
-            ['id' => 2, 'title' => 'JS', 'description' => 'This Is description', 'posted_by' => 'Mohamed', 'created_at' => '2021-03-25'],
-        ];
+        $posts = Post::all();
+        // dd($posts);
 
         return view('posts.index', [
             'posts' => $posts,
@@ -20,7 +20,11 @@ class PostController extends Controller
 
     public function show($post)
     {
-        $post = ['id' => 1, 'title' => 'Laravel', 'description' => 'Show Post Description', 'posted_by' => 'Ahmed', 'created_at' => '2021-03-13'];
+        // dd($post);
+        $post = Post::find($post);
+        dd($post);
+        // dd($post->get());  // ?????????????????????????????????? ليه بيرجع كله 
+        // $post = ['id' => 1, 'title' => 'Laravel', 'description' => 'Show Post Description', 'posted_by' => 'Ahmed', 'created_at' => '2021-03-13'];
       //dd($post);
         return view('posts.show', [
             'post' => $post
@@ -29,12 +33,35 @@ class PostController extends Controller
 
     public function create()
     {
-        return view('posts.create');
+        return view('posts.create',[
+            'users' => User::all()
+        ]);
     }
 
-    public function store()
+    public function store(Request $myRequestObject)
     {
-        //logic for saving in db
+        dd($myRequestObject);                //  ????????????????????????????? بترجع ايه 
+        // dd($myRequestObject->all());
+        $data = $myRequestObject->all();
+        //$data = request()->all();
+        // request()->title == $data['title']
+
+        Post::create($data); // full the fillable only 
+
+        // Post::create($myRequestObject->all());
+
+        // Post::create([
+        //     'title' => $data['title'],
+        //     'description' => $data['description'],
+        //     'id' => 1, //those will be ignore cause they aren't in fillable
+        //     'ajsnhdoiqwjsd' => 'aikoshdiahsdui' //those will be ignore cause they aren't in fillable
+        // ]);
+
+        //with this syntax you don't need fillable
+        // $post = new Post;
+        // $post->title = $data['title'];
+        // $post->description = $data['description'];
+        // $post->save();
 
         return redirect()->route('posts.index');
     }
