4b527e932892f5a2c4e8c198e033b685db170be2
mohamedmagdycivily <mohamedmagdycivily@gmail.com>
2021-03-21 21:01:35 +0200

    add postResource

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
+
+// Route::get('/posts', function(Request $request){
+//     return "hello world";
+// });
